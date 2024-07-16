f0e9c98de3042894e68a21c2dc7162f6817f9cac
simaoazeitona <166546905+simaoazeitona@users.noreply.github.com>
2024-07-16 13:30:51 +0100

    Menu Feito

--- app/src/main/java/com/example/pam_projetofinal/Menu.java
+++ app/src/main/java/com/example/pam_projetofinal/Menu.java
@@ -12,5 +12,50 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Menu extends AppCompatActivity {
 
+    //Criar variaveis
+    TextView tv_nome;
+    Button sobre, comecar, comojogar;
+    SharedPreferences sp;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        // Ir buscar os Buttons do layout e colocar nas variaveis
+        sobre = findViewById(R.id.bt_sobre);
+        comecar = findViewById(R.id.bt_comecar);
+        comojogar = findViewById(R.id.bt_comojogar);
+
+        sobre.setOnClickListener(new View.OnClickListener() {//Iniciar Button sobre
+            @Override
+            public void onClick(View view)
+            {
+                //Abrir a Activity Sobre
+                Intent i = new Intent (Menu.this, Sobre.class);
+                startActivity(i);
+            }
+        });
+
+        comecar.setOnClickListener(new View.OnClickListener() { //Iniciar Button comecar
+            @Override
+            public void onClick(View view)
+            {
+                //Abrir a Activity Cara_Coroa
+                Intent i = new Intent (Menu.this, Cara_Coroa.class);
+                startActivity(i);
+            }
+        });
+
+        comojogar.setOnClickListener(new View.OnClickListener() { //Iniciar Button comojogar
+            @Override
+            public void onClick(View view)
+            {
+                //Abrir a Activity Ajuda
+                Intent i = new Intent (Menu.this, Ajuda.class);
+                startActivity(i);
+            }
+        });
+
+    }
 }
